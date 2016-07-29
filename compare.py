fc29816d64b25430d5935dd512a26aff4c7c34a2
Michael Van Delft <Michael@hybr.id.au>
2016-07-29 15:41:12 +0800

    Basic program works

--- compare.py
+++ compare.py
@@ -13,9 +13,19 @@ FIRST_DIRECTORY = '/home/michael/directory-old-files'
 # Directory with new files.
 SECOND_DIRECTORY = '/home/michael/directory-new-files'
 
+list_of_files = set()
+new_files = []
 
 for dirpath, dirnames, filenames in os.walk(FIRST_DIRECTORY):
     for filename in filenames:
-        full_path = os.path.join(dirpath, filename)
-        print(full_path)
-        print(hashlib.md5(open(full_path, 'rb').read()).hexdigest())
+        file_path = os.path.join(dirpath, filename)
+        file_hash = hashlib.md5(open(file_path, 'rb').read()).hexdigest()
+        list_of_files.add(file_hash)
+
+for dirpath, dirnames, filenames in os.walk(SECOND_DIRECTORY):
+    for filename in filenames:
+        file_path = os.path.join(dirpath, filename)
+        file_hash = hashlib.md5(open(file_path, 'rb').read()).hexdigest()
+        if file_hash not in list_of_files:
+            print(file_path)
+            new_files.append(file_path)
