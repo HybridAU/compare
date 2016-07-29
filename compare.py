1b5a12ef8b0c2bd2a0ea39e1d45731de66a42557
Michael Van Delft <Michael@hybr.id.au>
2016-07-29 15:58:59 +0800

    I'm happy with how it works.

--- compare.py
+++ compare.py
@@ -13,19 +13,33 @@ FIRST_DIRECTORY = '/home/michael/directory-old-files'
 # Directory with new files.
 SECOND_DIRECTORY = '/home/michael/directory-new-files'
 
-list_of_files = set()
+original_files = set()
 new_files = []
 
-for dirpath, dirnames, filenames in os.walk(FIRST_DIRECTORY):
-    for filename in filenames:
-        file_path = os.path.join(dirpath, filename)
-        file_hash = hashlib.md5(open(file_path, 'rb').read()).hexdigest()
-        list_of_files.add(file_hash)
 
-for dirpath, dirnames, filenames in os.walk(SECOND_DIRECTORY):
+# Run through the original directory and creates an md5 sum each of the files.
+# md5 is inscure because of known hash colisions, however because we are not
+# trying to validate the file's contents so it's good enough, faster and more
+# memory efficent than SHA256.
+for dirpath, dirnames, filenames in os.walk(ORIGINAL_DIRECTORY):
     for filename in filenames:
         file_path = os.path.join(dirpath, filename)
         file_hash = hashlib.md5(open(file_path, 'rb').read()).hexdigest()
-        if file_hash not in list_of_files:
+        original_files.add(file_hash)
+
+
+# Run through the new directory and print files who's md5 hash is not in the
+# original list of files.
+for dirpath, dirnames, filenames in os.walk(NEW_DIRECTORY):
+    for filename in filenames:
+        file_path = os.path.join(dirpath, filename)
+        file_hash = hashlib.md5(open(file_path, 'rb').read()).hexdigest()
+        if file_hash not in original_files:
             print(file_path)
             new_files.append(file_path)
+
+
+# Print the list of new files out.
+with open('new-files.txt', 'w') as output_file:
+    for new_file in new_files:
+        output_file.write("%s\n" % new_file)
